2312462a9061742e5b5334643b0a56243c2e420e
Ciaran Grant <Ciarangrant2@aol.co.uk>
2024-02-28 21:02:03 +0000

    added burger menu functionality

--- Frontend/src/Navbar/Navbar.tsx
+++ Frontend/src/Navbar/Navbar.tsx
@@ -1,18 +1,31 @@
 import './Navbar.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
-export default function Navbar(){
-    return(
+export default function Navbar() {
+    // State to hold if the burger nav is open or closed
+    const [isNavOpen, setIsNavOpen] = useState(false)
+    // Create a function to toggle the burger nav list to the opposite of what it currently is
+    const openNav = () => {
+        setIsNavOpen(!isNavOpen)
+    }
+
+    return (
         <nav>
             <div className='leftNav'>
                 <a>Portfolio</a>
             </div>
             <div className='rightNav'>
-                <a>Menus</a>
-                <div className='navBurgerIcon'>
+                {/* <p>Menus</p> */}
+                <div className='navBurgerIcon' onClick={openNav}>
                     <FontAwesomeIcon icon={faBars} />
                 </div>
+                {isNavOpen && (
+                <div className={`menuContent ${isNavOpen ? 'active' : ''}`}>
+                    <a href="">Home</a>
+                </div>
+            )}
             </div>
         </nav>
     )
