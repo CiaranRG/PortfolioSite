34e34d6ed0689a40de64c92e15853c0fadd488cd
Ciaran Grant <Ciarangrant2@aol.co.uk>
2024-04-27 09:02:32 +0100

    Added formspree to the contact section

--- Frontend/src/Components/ContactForm/ContactForm.tsx
+++ Frontend/src/Components/ContactForm/ContactForm.tsx
@@ -1,8 +1,15 @@
 import './ContactForm.scss'
 import { useState, ChangeEvent } from "react";
+// Importing these for use with formspree
+import { useForm, ValidationError } from '@formspree/react';
 
 export default function ContactForm() {
-    const [formInfo, setFormInfo] = useState({name: '', email: '', message: ''})
+    const [formInfo, setFormInfo] = useState({ name: '', email: '', message: '' })
+    // This is the state for our form handling
+    const [state, handleSubmit] = useForm("xleqzrea");
+    if (state.succeeded) {
+        return <p className='contactSuccess'>Your message has been sent!</p>;
+    }
 
     const handleChange = (evt: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = evt.target
@@ -21,18 +28,25 @@ export default function ContactForm() {
         <>
             <div className="contactFormBg" onClick={handleModalClick}>
                 <h1>Contact Me!</h1>
-                <div className='contactSection'>
-                    <label htmlFor="">Name</label>
-                    <input type="text" value={formInfo.name} name='name' onChange={handleChange} placeholder='Name' required/>
-                </div>
-                <div className='contactSection'>
-                    <label htmlFor="">Email</label>
-                    <input type="email" value={formInfo.email} name='email' onChange={handleChange} placeholder='Email' required/>
-                </div>
-                <div className='contactSection'>
-                    <label htmlFor="">Message</label>
-                    <textarea id="message" cols={30} rows={5} value={formInfo.message} name='message' onChange={handleChange} required></textarea>
-                </div>
+                <form onSubmit={handleSubmit}>
+                    <div className='contactSection'>
+                        <label htmlFor='name'>Name</label>
+                        <input type="text" value={formInfo.name} id='name' name='name' onChange={handleChange} placeholder='Name' required />
+                    </div>
+                    <div className='contactSection'>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" value={formInfo.email} id='email' name='email' onChange={handleChange} placeholder='Email' required />
+                        <ValidationError prefix="Email" field="email" errors={state.errors} />
+                    </div>
+                    <div className='contactSection'>
+                        <label htmlFor="message">Message</label>
+                        <textarea cols={30} rows={5} value={formInfo.message} id="message" name='message' onChange={handleChange} required></textarea>
+                        <ValidationError prefix="Message" field="message" errors={state.errors} />
+                    </div>
+                    <div className="contactButtonDiv">
+                        <button type='submit' className='contactSubmit' disabled={state.submitting}>Submit</button>
+                    </div>
+                </form>
             </div>
         </>
     )
